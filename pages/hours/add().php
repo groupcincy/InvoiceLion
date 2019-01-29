ca631c306bb69f207ff816ccbdad97fceeb4b2c6
Maurits van der Schee <maurits@vdschee.nl>
2019-01-30 00:22:46 +0100

    update

--- pages/hours/add().php
+++ pages/hours/add().php
@@ -6,12 +6,9 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$data = $_POST;
 
 	if ($data['hours']['add_customer']) {
-		$customer_id = DB::insert('INSERT INTO `customers` (`tenant_id`, `name`) VALUES (?, ?)', $_SESSION['user']['tenant_id'], $data['hours']['add_customer']);
+		$data['hours']['customer_id'] = DB::insert('INSERT INTO `customers` (`tenant_id`, `name`) VALUES (?, ?)', $_SESSION['user']['tenant_id'], $data['hours']['add_customer']);
+		$data['hours']['add_customer'] = null;
 		$customers = DB::selectPairs('select `id`,`name` from `customers`  WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
-		$customer_added = true;
-	} else {
-		$customer_id = $data['hours']['customer_id'];
-		$customer_added = false;
 	}
 
 	if (!$data['hours']['project_id']) $data['hours']['project_id']=NULL;
@@ -21,10 +18,10 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	if (!$data['hours']['hours_worked']) $errors['hours[hours_worked]']='Hours worked not set';	
 	if (!$data['hours']['hourly_fee']) $errors['hours[hourly_fee]']='Hourly fee not set';	
 	if (!$data['hours']['vat_percentage']) $errors['hours[vat_percentage]']='VAT percentage not set';	
-	if (!$customer_id) $errors['hours[customer_id]']='Customer not set';	
+	if (!$data['hours']['customer_id']) $errors['hours[customer_id]']='Customer not set';	
 	
 	//set vat_percentage to NULL if the customer has vat_reverse_charge
-	$vat_reverse_charge = DB::selectValue('select `vat_reverse_charge` from `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $customer_id);
+	$vat_reverse_charge = DB::selectValue('select `vat_reverse_charge` from `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $data['hours']['customer_id']);
 	if ($vat_reverse_charge) $data['hours']['vat_percentage']=NULL;
 	
 	if (!isset($errors)) {
@@ -33,9 +30,9 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 			if($data['hours']['vat_percentage']) $total = $subtotal*((100+$data['hours']['vat_percentage'])/100); 
 			else $total = $subtotal;
 
-			$invoiceline_id = DB::insert('INSERT INTO `invoicelines` (`tenant_id`, `customer_id`, `name`, `subtotal`, `vat`,`vat_percentage`,`total`) VALUES (?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $customer_id, $data['hours']['name'], $subtotal, ($total - $subtotal), $data['hours']['vat_percentage'], $total);
+			$invoiceline_id = DB::insert('INSERT INTO `invoicelines` (`tenant_id`, `customer_id`, `name`, `subtotal`, `vat`,`vat_percentage`,`total`) VALUES (?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['hours']['customer_id'], $data['hours']['name'], $subtotal, ($total - $subtotal), $data['hours']['vat_percentage'], $total);
 
-			$hour_id = DB::insert('INSERT INTO `hours` (`tenant_id`, `customer_id`, `project_id`, `date`, `name`, `hours_worked`, `hourly_fee`, `subtotal`, `vat_percentage`, `type`, `comment`, `invoiceline_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $customer_id, $data['hours']['project_id'], $data['hours']['date'], $data['hours']['name'], $data['hours']['hours_worked'], $data['hours']['hourly_fee'], $subtotal, $data['hours']['vat_percentage'], $data['hours']['type'], $data['hours']['comment'], $invoiceline_id);
+			$hour_id = DB::insert('INSERT INTO `hours` (`tenant_id`, `customer_id`, `project_id`, `date`, `name`, `hours_worked`, `hourly_fee`, `subtotal`, `vat_percentage`, `type`, `comment`, `invoiceline_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['hours']['customer_id'], $data['hours']['project_id'], $data['hours']['date'], $data['hours']['name'], $data['hours']['hours_worked'], $data['hours']['hourly_fee'], $subtotal, $data['hours']['vat_percentage'], $data['hours']['type'], $data['hours']['comment'], $invoiceline_id);
 
 			if ($invoiceline_id && $hour_id) {
  				Flash::set('success','Hours saved');
@@ -46,10 +43,6 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 			$error = 'Hours not saved: '.$e->getMessage();
 		}
 	}	
-	if($customer_added == true) {
-		//try to remove the created customer - no error handling
-		$rows = DB::delete('DELETE FROM `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $customer_id);
-	}
 } else {
 	$data = array('hours'=>array(
 		'customer_id'=>NULL, 
