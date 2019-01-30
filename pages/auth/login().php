b81b6af2e2bb46bd7a05f0e061714c15ac422c25
Maurits van der Schee <maurits@vdschee.nl>
2019-01-30 21:57:14 +0100

    update

--- pages/auth/login().php
+++ pages/auth/login().php
@@ -1,6 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD']=='POST') {
-	$data = $_POST;
+  $data = $_POST;
+  $error = "Username/password combination not valid";
   $errors['username'] = "Username/password combination not valid";
   $errors['password'] = "Username/password combination not valid";
   if (Auth::login($data['username'],$data['password'])) {
