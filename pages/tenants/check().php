1e9ea7af4eb4e1619d93d4ec62932e4c67577c33
Maurits van der Schee <maurits@vdschee.nl>
2019-01-31 16:12:42 +0100

    update

--- pages/tenants/check().php
+++ pages/tenants/check().php
@@ -3,4 +3,5 @@
 $results = array(
     'Hours: hours worked * hourly fee = subtotal'=>DB::selectValues('select id from hours where tenant_id=? and ROUND(subtotal - (hours_worked * hourly_fee))<>0', $_SESSION['user']['tenant_id']),
     'Hours: hours.subtotal = invoicelines.subtotal'=>DB::selectValues('select hours.id from hours, invoicelines where hours.tenant_id=? and hours.invoiceline_id = invoicelines.id and hours.subtotal <> invoicelines.subtotal', $_SESSION['user']['tenant_id']),
+    'Hours: hours.vat_percentage = invoicelines.vat_percentage'=>DB::selectValues('select hours.id from hours, invoicelines where hours.tenant_id=? and hours.invoiceline_id = invoicelines.id and hours.vat_percentage <> invoicelines.vat_percentage', $_SESSION['user']['tenant_id']),
 );
