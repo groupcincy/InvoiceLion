e3b9babaff1097ccac21bb8901af84640cdc8fcf
Joost van der Schee <jhvanderschee@gmail.com>
2019-01-31 20:49:18 +0100

    update

--- pages/invoices/edit($id).php
+++ pages/invoices/edit($id).php
@@ -16,6 +16,13 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	if (!isset($errors)) {
 		try {
 			$rowsAffected = DB::update('UPDATE `invoices` SET `name`=?, `date`=?, `sent`=?, `paid`=?, `reminder1`=?, `reminder2`=?, `customer_id`=? WHERE `tenant_id` = ? AND `id` = ?', $data['invoices']['name'], $data['invoices']['date'], $data['invoices']['sent'], $data['invoices']['paid'], $data['invoices']['reminder1'], $data['invoices']['reminder2'], $invoice['invoices']['customer_id'], $_SESSION['user']['tenant_id'], $id);
+			
+			//disconnect all invoicelines from this invoice
+			foreach ($invoicelines as $invoiceline) $rowsAffected = DB::update('UPDATE `invoicelines` SET `invoice_id`= NULL WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $invoiceline['invoicelines']['id']);
+
+			//connect selected invoicelines to this invoice
+			foreach ($data['invoiceline_id'] as $value) $rowsAffected = DB::update('UPDATE `invoicelines` SET `invoice_id`=? WHERE `tenant_id` = ? AND `id` = ?', $id, $_SESSION['user']['tenant_id'], $value);
+			
 			if ($rowsAffected!==false) {
 				Flash::set('success','Invoices saved');
 				Router::redirect('invoices/view/'.$id);
