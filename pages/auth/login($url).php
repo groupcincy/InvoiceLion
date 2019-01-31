1f8d7ca6cea1626df9d597d0f03ba6c1518e290c
Maurits van der Schee <maurits@vdschee.nl>
2019-01-31 16:56:58 +0100

    update

---
+++ pages/auth/login($url).php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER['REQUEST_METHOD']=='POST') {
+  $data = $_POST;
+  $error = "Username/password combination not valid";
+  $errors['username'] = "Username/password combination not valid";
+  $errors['password'] = "Username/password combination not valid";
+  if (Auth::login($data['username'],$data['password'])) {
+    Router::redirect($url);
+  }
+}
