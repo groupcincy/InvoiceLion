5ba7620c7fa57b3910fc8501c9908f8720746119
Joost van der Schee <jhvanderschee@gmail.com>
2019-02-01 18:05:12 +0100

    update

---
+++ pages/settings/invoice/view().php
@@ -0,0 +1,6 @@
+<?php 
+$data = DB::selectOne('SELECT * from `tenants` WHERE `id` = ?', $_SESSION['user']['tenant_id']);
+$projects = DB::select('select `id`,`name`,`customer_id` from `projects` WHERE `tenant_id` = ? and `active` ORDER BY name', $_SESSION['user']['tenant_id']);
+$hourtypes = DB::select('select * from `hourtypes` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
+$subscriptiontypes = DB::select('select * from `subscriptiontypes` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
+$data['tenants']['timetracking']=1;
