639ed0d5d80195f0d647526931d26c38ebb6b5e4
Joost van der Schee <jhvanderschee@gmail.com>
2019-02-02 20:59:56 +0100

    update

--- pages/subscriptions/add().php
+++ pages/subscriptions/add().php
@@ -11,6 +11,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		$customers = DB::selectPairs('select `id`,`name` from `customers`  WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
 	}
 
+	//set vat_percentage to NULL if the customer has vat_reverse_charge
+	$vat_reverse_charge = DB::selectValue('select `vat_reverse_charge` from `customers` WHERE `tenant_id` = ? AND `id` = ?', $_SESSION['user']['tenant_id'], $data['subscriptions']['customer_id']);
+	if ($vat_reverse_charge) $data['subscriptions']['vat_percentage']=NULL;
+
     if (!$data['subscriptions']['from']) $errors['subscriptions[from]'] = 'Date not set';
     if (!$data['subscriptions']['name']) $errors['subscriptions[name]'] = 'Name not set';
     if (!$data['subscriptions']['fee']) $errors['subscriptions[fee]'] = 'Fee not set';
@@ -23,8 +27,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (!isset($errors)) {
         try {
             $subscription_id = DB::insert('INSERT INTO `subscriptions` (`tenant_id`, `fee`, `vat_percentage`, `months`, `name`, `from`, `comment`, `subscriptiontype_id`, `customer_id`, `project_id`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['subscriptions']['fee'], $data['subscriptions']['vat_percentage'], $data['subscriptions']['months'], $data['subscriptions']['name'], $data['subscriptions']['from'], $data['subscriptions']['comment'], $data['subscriptions']['subscriptiontype_id'], $data['subscriptions']['customer_id'], $data['subscriptions']['project_id']);
-
-            $until = date('Y-m-d', strtotime($data['subscriptions']['from'] . ' +'.$period.' months - 1 day'));
+            
+            //get the until date
+            $until = date('Y-m-d', strtotime($data['subscriptions']['from'] . ' +'.$data['subscriptions']['months'].' months - 1 day'));
             //add first subscriptionperiod
             $subscriptionperiod_id = DB::insert('INSERT INTO `subscriptionperiods` (`tenant_id`, `from`, `until`, `name`, `subscription_id`, `comment`,`invoiceline_id`) VALUES (?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['subscriptions']['from'], $until, $data['subscriptions']['name'], $subscription_id, NULL , NULL);
 
