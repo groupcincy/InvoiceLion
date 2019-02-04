c4163e7d07f8c4cda7d0ad62d1600873ce573afa
Joost van der Schee <jhvanderschee@gmail.com>
2019-02-04 23:09:14 +0100

    update

--- pages/auth/register().php
+++ pages/auth/register().php
@@ -1,6 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD']=='POST') {
     $data = $_POST;
+    $country = $data['country'];
+    $countries = array("nl");
+    if (!in_array($country,$countries)) {
+        $errors['country'] = "Invalid country";
+    }
     $username = $data['username'];
     if (!$username) {
         $errors['username'] = "Username cannot be empty";
@@ -8,11 +13,15 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
         $errors['username'] = "Username is not a valid email address";
     } elseif (Auth::exists($username)) {
         $errors['username'] = "Username is already taken";
-    } else {
+    } 
+    if (!isset($errors)) {
         $error = "User can not be registered";
         $userId = NoPassAuth::register($username);
         if ($userId) {
-            $tenantId = DB::insert('INSERT INTO `tenants` (`name`, `email`, `invoice_email`) VALUES (?, ?, ?)', $username, $username, $username); // does not need tenant_id check
+            $tenantId = DB::insert('INSERT INTO `tenants` (`name`, `email`, `invoice_email`, `country`) VALUES (?, ?, ?, ?)', $username, $username, $username, $country); // does not need tenant_id check
+            foreach(array('invoice_styles', 'invoice_template', 'invoiceline_template', 'invoice_page_number') as $field) {
+                DB::update('UPDATE `tenants` SET `'.$field.'` = ? WHERE `id` = ?', file_get_contents("translations/$country/$field.txt"), $tenantId);
+            }
             if ($tenantId) {
                 $rowsAffected = DB::update('UPDATE `users` SET `tenant_id`=? WHERE `id` = ?', $tenantId, $userId);
                 if ($rowsAffected) {
@@ -30,5 +39,5 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
         }
     }
 } else {
-    $data = array('username' => '');
+    $data = array('username' => '', 'country' => 'nl');
 }
