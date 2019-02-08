369af60e0b46979c289e4e38e5e5775dcb9737e4
Joost van der Schee <jhvanderschee@gmail.com>
2019-02-08 11:48:37 +0100

    Update security.php

--- templates/security.php
+++ templates/security.php
@@ -2,6 +2,6 @@
 if (empty($_SESSION['user']['tenant_id'])) {
     if (explode('/',Router::getView()?:'/')[1]!='auth') {
         $url = urlencode(trim(Router::getRequest(),'/'));
-        Router::redirect('auth/login/'.$url);
+        Router::redirect('auth/login?url='.$url);
     }
 }
