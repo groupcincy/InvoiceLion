63f0b3b6bd07ff1d7fde2806b65302a7b04bb5bd
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-22 23:06:55 +0200

    update

--- pages/hours/index($order,$asc).php
+++ pages/hours/index($order,$asc).php
@@ -1,10 +1,8 @@
 <?php 
-if(!isset($order) || !in_array($order,['id','date','hours_worked','project_id','name','subtotal','invoice_id','type','comment'])) $order = 'date';
+if(!isset($order) || !in_array($order,['date','hours_worked','name','subtotal','customer'])) $order = 'date';
 if(!isset($asc) || !in_array($asc,['asc','desc'])) $asc = 'desc';
 
-$data = DB::select('select * from `hours` WHERE `tenant_id` = ? order by `'.$order.'` '.$asc.', `id` '.$asc.' limit 200', $_SESSION['user']['tenant_id']);
+if($order == 'customer') $data = DB::select('select * from `hours`,`customers` WHERE `invoices`.`tenant_id` = ? AND `hours`.customer_id = `customers`.id order by `customers`.name '.$asc, $_SESSION['user']['tenant_id']);
+else $data = DB::select('select * from `hours` WHERE `tenant_id` = ? order by `'.$order.'` '.$asc.', `id` '.$asc.' limit 200', $_SESSION['user']['tenant_id']);
 
-$invoices = DB::selectPairs('select `id`,`name` from `invoices` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
-$projects = DB::selectPairs('select `id`,`name` from `projects` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
 $customers = DB::selectPairs('select `id`,`name` from `customers` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
-$hourtypes = DB::selectPairs('select `id`,`name` from `hourtypes` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
--- pages/invoices/index($order,$asc).php
+++ pages/invoices/index($order,$asc).php
@@ -1,7 +1,8 @@
 <?php 
-if(!isset($order) || !in_array($order,['id','number','name','date','sent','paid','reminder1','reminder2','customer_id'])) $order = 'number';
+if(!isset($order) || !in_array($order,['id','number','name','date','sent','paid','reminder1','reminder2','customer'])) $order = 'number';
 if(!isset($asc) || !in_array($asc,['asc','desc'])) $asc = 'desc';
-if($order == 'paid') $order = 'paid';
 
-$data = DB::select('select * from `invoices` WHERE `tenant_id` = ? order by '.$order.' '.$asc, $_SESSION['user']['tenant_id']);
+if($order == 'customer') $data = DB::select('select * from `invoices`,`customers` WHERE `invoices`.`tenant_id` = ? AND `invoices`.customer_id = `customers`.id order by `customers`.name '.$asc, $_SESSION['user']['tenant_id']);
+else $data = DB::select('select * from `invoices` WHERE `tenant_id` = ? order by '.$order.' '.$asc, $_SESSION['user']['tenant_id']);
+
 $customers = DB::selectPairs('select `id`,`name` from `customers` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
