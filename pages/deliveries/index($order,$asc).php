b4d71a0fdcd41d70d2299a807c0490e32387f136
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-22 23:14:46 +0200

    update

--- pages/deliveries/index($order,$asc).php
+++ pages/deliveries/index($order,$asc).php
@@ -3,7 +3,7 @@ if(!isset($order) || !in_array($order,['date','name','subtotal','customer'])) $o
 if(!isset($asc) || !in_array($asc,['asc','desc'])) $asc = 'desc';
 
 if($order == 'customer') $data = DB::select('select * from `deliveries`,`customers` WHERE `deliveries`.`tenant_id` = ? AND `deliveries`.customer_id = `customers`.id order by `customers`.name '.$asc.' limit 200', $_SESSION['user']['tenant_id']);
-$data = DB::select('select * from `deliveries` WHERE `tenant_id` = ? order by `'.$order.'` '.$asc.', `id` '.$asc.' limit 200', $_SESSION['user']['tenant_id']);
+else $data = DB::select('select * from `deliveries` WHERE `tenant_id` = ? order by `'.$order.'` '.$asc.', `id` '.$asc.' limit 200', $_SESSION['user']['tenant_id']);
 
 $invoices = DB::selectPairs('select `id`,`name` from `invoices` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
 $projects = DB::selectPairs('select `id`,`name` from `projects` WHERE `tenant_id` = ?', $_SESSION['user']['tenant_id']);
