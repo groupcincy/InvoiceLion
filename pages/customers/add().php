cdd3c7ac20d9f771899905bea712d34bcd049993
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-26 02:48:39 +0200

    update

--- pages/customers/add().php
+++ pages/customers/add().php
@@ -4,7 +4,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	if (!isset($errors)) {
 		if(!$data['customers']['tax_reverse_charge']) $data['customers']['tax_reverse_charge'] = 0;
 		try {
-			$id = DB::insert('INSERT INTO `customers` (`tenant_id`, `name`, `email`, `contact`, `address`, `tax_reverse_charge`) VALUES (?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['customers']['name'], $data['customers']['email'], $data['customers']['contact'], $data['customers']['address'], $data['customers']['tax_reverse_charge']);
+			$id = DB::insert('INSERT INTO `customers` (`tenant_id`, `name`, `email`, `contact`, `address`, `tax_reverse_charge`, `language_id`) VALUES (?, ?, ?, ?, ?, ?, ?)', $_SESSION['user']['tenant_id'], $data['customers']['name'], $data['customers']['email'], $data['customers']['contact'], $data['customers']['address'], $data['customers']['tax_reverse_charge'], $data['customers']['language_id']);
 			if ($id) {
 				Flash::set('success','Customer saved');
 				Router::redirect('customers/index');
@@ -15,6 +15,6 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 		}
 	}	
 } else {
-	$data = array('customers'=>array('name'=>NULL, 'email'=>NULL, 'contact'=>NULL, 'address'=>NULL, 'tax_reverse_charge'=>NULL));
+	$data = array('customers'=>array('name'=>NULL, 'email'=>NULL, 'contact'=>NULL, 'address'=>NULL, 'tax_reverse_charge'=>NULL, 'language_id'=>NULL));
 	$languages = DB::selectPairs('SELECT `id`,`name` FROM `languages`'); // tenant_id not required
 }
