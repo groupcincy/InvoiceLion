58a48ffb586bbbd171d6aeaea3ae7b3480593724
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-26 22:38:58 +0200

    update

--- pages/settings/invoice/templatetabs.php
+++
@@ -1,5 +0,0 @@
-<ul class="tabs">
-<?php foreach ($languages as $language): ?>
-    <li><a href="/settings/languages/view/<?php e($language['languages']['id']);?>">Template <?php e(strtoupper($language['languages']['code']));?></a></li>
-<?php endforeach;?>
-</ul>
