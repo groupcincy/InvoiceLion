3b3d3053fecf1d1cd43334f22e2fe1f5e01da986
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-26 22:25:39 +0200

    update

--- pages/settings/settingstabs.php
+++ pages/settings/settingstabs.php
@@ -1,6 +1,6 @@
 <ul class="tabs">
     <li <?php if(substr(Router::getRequest(),0,17)==='/settings/general') e('class=active'); ?>><a href="/settings/general/view">General</a></li>
-    <li><a href="/settings/company/view">Company</a></li>
-    <li><a href="/settings/invoice/view">Invoice</a></li>
+    <li <?php if(substr(Router::getRequest(),0,17)==='/settings/company') e('class=active'); ?>><a href="/settings/company/view">Company</a></li>
+    <li <?php if(substr(Router::getRequest(),0,17)==='/settings/invoice') e('class=active'); ?>><a href="/settings/invoice/view">Invoice</a></li>
 </ul>
 
