1b7e1f27ced4081faeb223ea949e560e8264dfec
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-29 13:52:31 +0200

    update

--- pages/projects/edit($id).php
+++ pages/projects/edit($id).php
@@ -5,6 +5,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	if (!isset($customers[$data['projects']['customer_id']])) $errors['projects[customer_id]']='Customer not found';
 	if (!isset($errors)) {
 		try {
+			if($data['projects']['default_hourly_fee']=='0.00') $data['projects']['default_hourly_fee'] = NULL;
 			$rowsAffected = DB::update('UPDATE `projects` SET `name`=?, `customer_id`=?, `default_hourly_fee`=?, `active`=? WHERE `tenant_id` = ? AND `id` = ?', $data['projects']['name'], $data['projects']['customer_id'], $data['projects']['default_hourly_fee'], $data['projects']['active'], $_SESSION['user']['tenant_id'], $id);
 			if ($rowsAffected!==false) {
 				Flash::set('success','Project saved');
