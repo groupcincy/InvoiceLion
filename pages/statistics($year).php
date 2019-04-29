f8b26da9dc4c0128e223ac93ed8f24d2e92f8c83
Joost van der Schee <jhvanderschee@gmail.com>
2019-04-29 14:17:57 +0200

    update

---
+++ pages/statistics($year).php
@@ -0,0 +1,16 @@
+<?php 
+if(!isset($year)) $year == date("Y");
+
+$subscriptionIncome = DB::selectValue('SELECT SUM((fee*(12/months))/12) FROM subscriptions WHERE `tenant_id` = ? AND canceled IS NULL', $_SESSION['user']['tenant_id']);
+
+$incomeThisYear = DB::selectValue('SELECT sum(subtotal) FROM invoices WHERE `tenant_id` = ? AND `sent` and YEAR(`date`) = ?', $_SESSION['user']['tenant_id'],$year);
+
+$hoursAddon = DB::selectValue('SELECT sum(subtotal) FROM `hours` WHERE `tenant_id` = ? AND `invoiceline_id` IS NULL', $_SESSION['user']['tenant_id']);
+$deliveryAddon = DB::selectValue('SELECT sum(subtotal) FROM `deliveries` WHERE `tenant_id` = ? AND `invoiceline_id` IS NULL', $_SESSION['user']['tenant_id']);
+$incomeThisYearAddon = $hoursAddon + $deliveryAddon;
+
+$incomeLastYear = DB::selectValue('SELECT sum(subtotal) FROM invoices WHERE `tenant_id` = ? AND `sent` and YEAR(`date`) = ?', $_SESSION['user']['tenant_id'],($year-1));
+
+$sumhourspertype_thisyear = DB::selectPairs('SELECT `hourtypes`.name,SUM(hours_worked) as `hourtypes.sum` FROM `hours` LEFT JOIN `hourtypes` ON `hours`.`type` = `hourtypes`.id WHERE `hours`.`tenant_id` = ? AND year(hours.date) = ? GROUP BY `type`', $_SESSION['user']['tenant_id'],$year);
+arsort($sumhourspertype_thisyear);
+$totalhours_thisyear = array_sum($sumhourspertype_thisyear);
