980babd6815cc82af119345bac36562685bec2a6
Maurits van der Schee <maurits@vdschee.nl>
2019-04-29 23:01:12 +0200

    improved error messages on pwd forgotten

--- pages/auth/forgotten().php
+++ pages/auth/forgotten().php
@@ -3,10 +3,10 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$data = $_POST;
   $username = $data['username'];
   $token = NoPassAuth::token($username);
-  $errors['username'] = 'Email not found';
-  if ($token) {
-    if (!Cache::get('AuthForgotten_mailto_'.$username)) {
-      Cache::set('AuthForgotten_mailto_'.$username,'1',NoPassAuth::$tokenValidity);
+  $errors['username'] = 'Email too recently used';
+  if (!Cache::get('AuthForgotten_mailto_'.$username)) {
+    Cache::set('AuthForgotten_mailto_'.$username,'1',NoPassAuth::$tokenValidity);
+    if ($token) {
       mail($username,'Login to '.Router::getBaseUrl(),'Click here: '.Router::getBaseUrl()."auth/reset/$token");
     }
     Router::redirect("auth/sent");
