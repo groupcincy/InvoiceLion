559c474111cccf5e94560ce57b18c5166a524be6
Joost van der Schee <jhvanderschee@gmail.com>
2019-05-07 11:23:44 +0200

    update

--- templates/admin.php
+++ templates/admin.php
@@ -1,3 +1,4 @@
 <?php
 $flash = Flash::get();
 $tenant = DB::selectOne('select * from `tenants` WHERE `id` = ?', $_SESSION['user']['tenant_id']);
+$currency = DB::selectOne('select * from `currencies` WHERE `id` = ?', $tenant['tenants']['currency_id']);
