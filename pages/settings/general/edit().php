539c33a543b612a504ae0904f5430d337f524a8e
Joost van der Schee <jhvanderschee@gmail.com>
2019-05-07 12:06:22 +0200

    update

--- pages/settings/general/edit().php
+++ pages/settings/general/edit().php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	$data = $_POST;
 	if (!isset($errors)) {
 		try {
-			$rowsAffected = DB::update('UPDATE `tenants` SET `default_tax_percentage`=?, `default_hourly_fee`=?, `subscriptions_active`=?, `hours_active`=? WHERE `id` = ?', $data['tenants']['default_tax_percentage'], $data['tenants']['default_hourly_fee'], $data['tenants']['subscriptions_active'], $data['tenants']['hours_active'], $_SESSION['user']['tenant_id']);
+			$rowsAffected = DB::update('UPDATE `tenants` SET `currency_id`=?, `default_tax_percentage`=?, `default_hourly_fee`=?, `subscriptions_active`=?, `hours_active`=? WHERE `id` = ?', $data['tenants']['currency_id'],$data['tenants']['default_tax_percentage'], $data['tenants']['default_hourly_fee'], $data['tenants']['subscriptions_active'], $data['tenants']['hours_active'], $_SESSION['user']['tenant_id']);
 			if ($rowsAffected!==false) {
 				Flash::set('success','General settings saved');
 				Router::redirect('settings/general/view');
@@ -15,5 +15,6 @@ if ($_SERVER['REQUEST_METHOD']=='POST') {
 	}
 } else {
 	$data = DB::selectOne('SELECT * from `tenants` WHERE `id` = ?', $_SESSION['user']['tenant_id']);
+	$currencies = DB::selectPairs('SELECT * from `currencies`');
 	$data['tenants']['timetracking']=1;
 }
